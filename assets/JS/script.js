c618369d8c19fbf6697f45368a5075be8f6e94e8
Qua Pham <qua.pham@amitgroup.vn>
2022-06-10 10:32:43 +0700

    demo code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,29 @@
-const dropdownBtn = document.querySelector(".dropdown-btn")
-const dropdownMenu = document.querySelector('.dropdown-content')
+// const dropdownBtn = document.querySelector(".dropdown-btn")
+// const dropdownMenu = document.querySelector('.dropdown-content')
 
-dropdownBtn.addEventListener( 'click', () => {
-dropdownMenu.classList.toggle("hide")
-}
-)
+// dropdownBtn.addEventListener( 'click', () => {
+// dropdownMenu.classList.toggle("hide")
+// }
+// )
+
+window.addEventListener("DOMContentLoaded", function () {
+  var setTimeOut;
+  document.querySelectorAll(".dropdown-btn").forEach((dr) => {
+    dr.addEventListener("click", function () {
+      console.log("CLICK");
+      clearTimeout(setTimeOut);
+      const subMenu = document.getElementById("dropdownContent");
+      if (subMenu.classList.contains("show")) {
+        subMenu.style.height = 0;
+        setTimeOut = setTimeout(() => {
+          subMenu.classList.remove("show");
+        }, 500);
+      } else {
+        subMenu.classList.add("show");
+        setTimeOut = setTimeout(function () {
+          subMenu.style.height = subMenu.scrollHeight + "px";
+        }, 0);
+      }
+    });
+  });
+});
